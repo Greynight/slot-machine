7f441bf4c9fbe334efcf0d96920e810df2610952
Ivan Sidorenko <i.sidorenko@plarium.com>
2019-08-23 11:30:09 +0300

    Updates

---
+++ precache-manifest.afa15c73a88f78615c9caeb1a383a3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654c9eb98257c33e75f020de47f8004d",
+    "url": "/slot-machine/index.html"
+  },
+  {
+    "revision": "2a557be6eec8ff79c7b4",
+    "url": "/slot-machine/static/css/main.88bdb0b7.chunk.css"
+  },
+  {
+    "revision": "29fbaab60acc5def4fe3",
+    "url": "/slot-machine/static/js/2.11181cc8.chunk.js"
+  },
+  {
+    "revision": "2a557be6eec8ff79c7b4",
+    "url": "/slot-machine/static/js/main.a48e2e1a.chunk.js"
+  },
+  {
+    "revision": "0d12287b37c209892ed0",
+    "url": "/slot-machine/static/js/runtime~main.7b41454a.js"
+  }
+]);
